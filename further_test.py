f9077bfdcb2d2d47fb395c1cbf9b74dbec4835cb
LanZhang-UCL <57351194+LanZhang-UCL@users.noreply.github.com>
2020-04-01 16:36:49 +0800

    Add files via upload

--- further_test.py
+++ further_test.py
@@ -3,15 +3,16 @@ from sklearn.metrics import accuracy_score
 
 # ======================================================================================================================
 # Further tests on B's prediction
-B_df = pd.read_csv('./B.csv')
+B_df = read_csv('./B.csv')
 y_true_B = B_df['label']
 y_pred_B = B_df['predict_label']
 y_score_B = B_df['score']
-print(accuracy_score())
+print(accuracy_score(y_true_B, y_pred_B))
 
 # ======================================================================================================================
 # Further tests on B's prediction
-D_df = pd.read_csv('./D.csv')
+D_df = read_csv('./D.csv')
 y_true_D = D_df['label']
 y_pred_D = D_df['predict_label']
 y_score_D = D_df['score']
+print(accuracy_score(y_true_D, y_pred_D))
