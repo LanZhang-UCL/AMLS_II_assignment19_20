c2de4537acfbbdaa5347ff1d37a4a707881a412d
LanZhang-UCL <57351194+LanZhang-UCL@users.noreply.github.com>
2020-04-02 19:10:16 +0800

    Add files via upload

--- Datasets/preprocessing.py
+++ Datasets/preprocessing.py
@@ -1,6 +1,34 @@
 import os
 import pandas as pd
 
+
+def content_process(content, topic):
+    # change capital letters to lower case letters
+    new_content = content.lower()
+    temp = topic.lower()
+
+    # remove special symbols
+    for a in '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n':
+        new_content = new_content.replace(a, '')
+        temp = temp.replace(a, '')
+
+    # replace URL as 'URL'
+    while new_content.find('http') != -1:
+        start = new_content.find('http')
+        end = start + 3
+        while end + 1 < len(new_content):
+            if new_content[end + 1] == ' ':
+                break
+            else:
+                end = end + 1
+        new_content = new_content.replace(new_content[start: end+1], 'URL')
+
+    # if topic is not in content, add it in front of content
+    if new_content.find(temp) == -1:
+        new_content = temp + ' ' + new_content
+    return new_content
+
+
 # Process raw training data and generate new training data.
 for root, _, files in os.walk('./Subtasks_BD/'):
     train_df = {'id':[], 'topic':[], 'label':[], 'content':[]}
@@ -18,8 +46,7 @@ for root, _, files in os.walk('./Subtasks_BD/'):
                     label = 0
                 train_df['id'].append(new_df['id'][i])
                 train_df['topic'].append(new_df['topic'][i])
-                content = new_df['topic'][i] + ' ' + new_df['content'][i]
-                train_df['content'].append(content.lower())
+                train_df['content'].append(content_process(new_df['content'][i], new_df['topic'][i]))
                 train_df['label'].append(label)
 
 train_df = pd.DataFrame(train_df)
@@ -40,8 +67,7 @@ for i in range(0, len(new_df['id'])):
             label = 0
         test_df['id'].append(new_df['id'][i])
         test_df['topic'].append(new_df['topic'][i])
-        content = new_df['topic'][i] + ' ' + new_df['content'][i]
-        test_df['content'].append(content.lower())
+        test_df['content'].append(content_process(new_df['content'][i], new_df['topic'][i]))
         test_df['label'].append(label)
 
 test_df = pd.DataFrame(test_df)
